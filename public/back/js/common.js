34f52116f6663f45ae278234db8de29492b71805
MaggieHsu <l>
2018-06-25 21:11:28 +0800

    后台页面建立并互相链接关联

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,3 +2,16 @@
  * Created by 阿呆 on 2018/6/25.
  */
 
+//进度条,要给所有的ajax用
+//ajax开始进度条开始,ajax结束进度条结束
+//第一个ajax开始时,发送进度条
+$(document).ajaxStart(function (){
+  NProgress.start();
+});
+$(document).ajaxStop(function (){
+  setTimeout(function(){
+    NProgress.done();
+  },1000)
+});
+
+

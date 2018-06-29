d2a7ede4944b4ed5c44f0c1bcce605b85e19de58
MaggieHsu <l>
2018-06-29 08:53:44 +0800

    分类加注释版

--- public/front/js/category.js
+++ public/front/js/category.js
@@ -2,7 +2,7 @@
  * Created by 阿呆 on 2018/6/29.
  */
 $(function (){
-
+//获取左侧一级分类名称
   $.ajax({
     type:"get",
     url:"/category/queryTopCategory",
@@ -10,20 +10,25 @@ $(function (){
     success:function (info){
       //console.log(info);
       $('.lt_category_left ul').html(template("tpl",info));
+
+      //跟据左侧第一个一级分类的第一个渲染第一个二级分类,id是固定不变的,当id=0被删除,第一个的id可能就是1,所以只能动态获取
       renderSecondById( info.rows[0].id );
     }
   });
 
-
+//给动态生成的a注册点击事件,用事件委托
   $('.lt_category_left').on("click","a",function(){
+    //获取点击的a的id
     var id =$(this).data("id");
+    //根据id渲染二级分类
     renderSecondById(id);
 
+    //给点击的a设置current样式,其余的a都取消current样式
     $(this).addClass("current").parent().siblings().find("a").removeClass("current");
   })
 
 
-
+//获取二级分类
 function renderSecondById (id) {
   $.ajax({
     type:"get",
